9ba2d106e6c6ef9d45581851e8dd0d2c5d51f5fc
GayathriYantra <gayathridayalan28.yantra@gmail.com>
2022-10-07 17:01:51 +0530

    Maven_Jenekins

---
+++ mavjenkins/src/test/java/jenkins/classroom1Test.java
@@ -0,0 +1,19 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class classroom1Test 
+{
+	@Test(groups="smoketest")
+	public void gitMethods()
+		{
+		// modified the code
+		System.out.println("Git Comments learning");
+		// providing the git command
+		System.out.println("Git comments done");
+		System.out.println("Created new branch");
+		//after merging the code with github
+		System.out.println("Created and merged with master branch");
+	}
+
+}
---
+++ mavjenkins/src/test/java/jenkins/classroom2Test.java
@@ -0,0 +1,19 @@
+package jenkins;
+
+import org.testng.annotations.Test;
+
+public class classroom2Test
+{
+	@Test(groups="regressiontest")
+	public void gitMethods()
+		{
+		// modified the code
+		System.out.println("Git Comments learning");
+		// providing the git command
+		System.out.println("Git comments done");
+		System.out.println("Created new branch");
+		//after merging the code with github
+		System.out.println("Created and merged with master branch");
+	}
+
+}
